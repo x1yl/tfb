f817e6e767f751e91d822ba543ae8edb7f89e8c3
xtyl <life23243@tutanota.com>
2023-11-07 21:18:35 -0500

    Added to allow user to only pick one reaction role

--- commands/utility/reaction.js
+++ commands/utility/reaction.js
@@ -1,23 +1,21 @@
 const { SlashCommandBuilder, PermissionsBitField } = require("discord.js");
-const { EmbedBuilder } = require('discord.js');
-const { mongoUri } = require('../../config.json');
+const { EmbedBuilder } = require("discord.js");
+const { mongoUri } = require("../../config.json");
 const { MongoClient } = require("mongodb");
 const uri = mongoUri;
 const mongodb = new MongoClient(uri);
 
-async function reactionRole(messageId, emo, role) {
+async function reactionRole(messageId, emo, role, max) {
   try {
-    const database = mongodb.db('Discord');
-    const collection = database.collection('reactionRole');
-    const doc = { messageId: messageId, emoji: emo, role: role };
+    const database = mongodb.db("Discord");
+    const collection = database.collection("reactionRole");
+    const doc = { messageId: messageId, emoji: emo, role: role, only: max };
     const result = await collection.insertOne(doc);
   } finally {
     // Ensures that the client will close when you finish/error
     await mongodb.close();
   }
 }
-
-
 
 module.exports = {
   category: "utility",
@@ -33,19 +31,27 @@ module.exports = {
     .addStringOption((option) =>
       option
         .setName("roles")
-        .setDescription("Enter the roles separated by spaces (in the same order as emojis)")
+        .setDescription(
+          "Enter the roles separated by spaces (in the same order as emojis)"
+        )
         .setRequired(true)
     )
     .addStringOption((option) =>
+      option.setName("title").setDescription("Title of embed").setRequired(true)
+    )
+    .addStringOption((option) =>
       option
-        .setName("title")
-        .setDescription("Title of embed")
+        .setName("1")
+        .setDescription("true (max 1) or false (no max)")
         .setRequired(true)
     ),
   async execute(interaction) {
-    const reactionEmojis = interaction.options.getString("reaction_emojis").split(" ");
+    const reactionEmojis = interaction.options
+      .getString("reaction_emojis")
+      .split(" ");
     const roles = interaction.options.getString("roles").split(" ");
     const title = interaction.options.getString("title");
+    const max = interaction.options.getString("1");
 
     if (
       interaction.member.permissions.has(
@@ -90,15 +96,21 @@ module.exports = {
       const messageId = message.id;
 
       for (let i = 0; i < reactionEmojis.length; i++) {
-        roleId = roles[i].replace(/\D/g, "")
-        reactionRole(messageId, reactionEmojis[i], roleId);
+        roleId = roles[i].replace(/\D/g, "");
+        reactionRole(messageId, reactionEmojis[i], roleId, max);
       }
 
       // Confirmation message
-      await interaction.followUp({content:"Reaction roles set up successfully.", ephemeral: true});
+      await interaction.followUp({
+        content: "Reaction roles set up successfully.",
+        ephemeral: true,
+      });
     } catch (error) {
       console.error(error);
-      await interaction.followUp({content: "An error occurred.", ephemeral: true});
+      await interaction.followUp({
+        content: "An error occurred.",
+        ephemeral: true,
+      });
     }
   },
 };
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Events, GatewayIntentBits, Partials, Collection } = require("discord.js");
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+} = require("discord.js");
 const { token, mongoUri } = require("./config.json");
 const { MongoClient } = require("mongodb");
 
@@ -72,6 +78,91 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
   const messageId = reaction.message.id;
 
   try {
+    // Connect to the database and retrieve the corresponding role documents for the emoji
+    await mongodb.connect();
+    const database = mongodb.db("Discord");
+    const collection = database.collection("reactionRole");
+
+    // Get all documents with the same messageId and emoji
+    const docs = await collection.find({ messageId }).toArray();
+
+    if (docs && docs.length > 0) {
+      const guild = reaction.message.guild;
+      const member = guild.members.cache.get(user.id);
+
+      if (member) {
+        for (const doc of docs) {
+          const role = doc.role;
+          if (emoji == doc.emoji) {
+            // Check if the user has the role
+            if (!member.roles.cache.has(role)) {
+              // If not, give them the role
+              member.roles.add(role).catch(console.error);
+            }
+          }
+
+          // Check if the document has "only" set to true
+          if (doc.only === "true") {
+            // Remove any other roles with the same messageId
+            const otherRoles = docs.filter(
+              (otherDoc) => otherDoc.emoji !== emoji
+            );
+            for (const otherDoc of otherRoles) {
+              const otherRole = otherDoc.role;
+              if (member.roles.cache.has(otherRole)) {
+                member.roles.remove(otherRole).catch(console.error);
+              }
+            }
+            const message = reaction.message;
+
+            try {
+              // Fetch all reactions on the message
+              const allReactions = message.reactions.cache;
+
+              // Iterate through all reactions and users
+              for (const [emoji, reactionInstance] of allReactions) {
+                const users = await reactionInstance.users.fetch();
+
+                // Check if it's not the current user's reaction
+                if (emoji !== reaction.emoji.name) {
+                  // Remove the current user's reaction from other users
+                  if (users.has(user.id)) {
+                    reactionInstance.users.remove(user.id);
+                  }
+                }
+              }
+            } catch (error) {
+              console.error("Error removing reactions:", error);
+            }
+          }
+        }
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await mongodb.close();
+  }
+});
+
+client.on(Events.MessageReactionRemove, async (reaction, user) => {
+  // When a reaction is received, check if the structure is partial
+  if (reaction.partial) {
+    // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the message:", error);
+      // Return as `reaction.message.author` may be undefined/null
+      return;
+    }
+  }
+  if (user.bot) return; // Ignore reactions from bots
+
+  const emoji = reaction.emoji.name; // Get the reacted emoji
+  const messageId = reaction.message.id;
+
+  try {
     // Connect to the database and retrieve the corresponding role for the emoji
     await mongodb.connect();
     const database = mongodb.db("Discord");
@@ -85,10 +176,7 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
 
       if (member) {
         // Check if the user has the role
-        if (!member.roles.cache.has(role)) {
-          // If not, give them the role
-          member.roles.add(role).catch(console.error);
-        }
+        member.roles.remove(role).catch(console.error);
       }
     }
   } catch (error) {
@@ -98,108 +186,66 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
   }
 });
 
-client.on(Events.MessageReactionRemove, async (reaction, user) => {
-	// When a reaction is received, check if the structure is partial
-	if (reaction.partial) {
-	  // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
-	  try {
-		await reaction.fetch();
-	  } catch (error) {
-		console.error("Something went wrong when fetching the message:", error);
-		// Return as `reaction.message.author` may be undefined/null
-		return;
-	  }
-	}
-	if (user.bot) return; // Ignore reactions from bots
-  
-	const emoji = reaction.emoji.name; // Get the reacted emoji
-	const messageId = reaction.message.id;
-  
-	try {
-	  // Connect to the database and retrieve the corresponding role for the emoji
-	  await mongodb.connect();
-	  const database = mongodb.db("Discord");
-	  const collection = database.collection("reactionRole");
-	  const doc = await collection.findOne({ messageId, emoji });
-  
-	  if (doc) {
-		const role = doc.role;
-		const guild = reaction.message.guild;
-		const member = guild.members.cache.get(user.id);
-  
-		if (member) {
-		  // Check if the user has the role
-		  member.roles.remove(role).catch(console.error);
+async function getMuteRole(guildId) {
+  const mongodb = new MongoClient(mongoUri);
 
-		}
-	  }
-	} catch (error) {
-	  console.error(error);
-	} finally {
-	  await mongodb.close();
-	}
-  });
+  try {
+    await mongodb.connect();
+    const db = mongodb.db("Discord");
+    const collection = db.collection("muteRoles");
 
+    const muteRoleDocument = await collection.findOne({ guildId: guildId });
 
-  async function getMuteRole(guildId) {
-	const mongodb = new MongoClient(mongoUri);
-  
-	try {
-	  await mongodb.connect();
-	  const db = mongodb.db('Discord');
-	  const collection = db.collection('muteRoles');
-  
-	  const muteRoleDocument = await collection.findOne({ guildId: guildId });
-  
-	  if (muteRoleDocument) {
-		return muteRoleDocument.muteRoleName;
-	  }
-  
-	  return null;
-	} finally {
-	  await mongodb.close();
-	}
+    if (muteRoleDocument) {
+      return muteRoleDocument.muteRoleName;
+    }
+
+    return null;
+  } finally {
+    await mongodb.close();
   }
-  async function unmuteUser(guildId, userId) {
-	const guild = client.guilds.cache.get(guildId);
-  
-	if (!guild) {
-	  return;
-	}
-  
-	const member = await guild.members.fetch(userId);
-	if (!member) {
-	  return;
-	}
-	const muteRoleName = await getMuteRole(guildId);
-	const mutedRole = guild.roles.cache.find(role => role.name === muteRoleName);
-	if (mutedRole) {
-	  await member.roles.remove(mutedRole);
-	}
-  }
-  
-  async function checkUnmuteTime() {
-	const now = Date.now();
-	const mongodb = new MongoClient(mongoUri);
-  
-	try {
-	  await mongodb.connect();
-	  const db = mongodb.db('Discord');
-	  const collection = db.collection('muted');
-  
-	  const cursor = collection.find({ unmuteTime: { $lte: now } });
-	  const entries = await cursor.toArray();
-  
-	  for (const entry of entries) {
-		await unmuteUser(entry.guildId, entry.userId);
-		await collection.deleteOne({ _id: entry._id });
-	  }
-	} finally {
-	  await mongodb.close();
-	}
-  }
-   setInterval(checkUnmuteTime, 1000)
+}
+async function unmuteUser(guildId, userId) {
+  const guild = client.guilds.cache.get(guildId);
 
+  if (!guild) {
+    return;
+  }
+
+  const member = await guild.members.fetch(userId);
+  if (!member) {
+    return;
+  }
+  const muteRoleName = await getMuteRole(guildId);
+  const mutedRole = guild.roles.cache.find(
+    (role) => role.name === muteRoleName
+  );
+  if (mutedRole) {
+    await member.roles.remove(mutedRole);
+  }
+}
+
+async function checkUnmuteTime() {
+  const now = Date.now();
+  const mongodb = new MongoClient(mongoUri);
+
+  try {
+    await mongodb.connect();
+    const db = mongodb.db("Discord");
+    const collection = db.collection("muted");
+
+    const cursor = collection.find({ unmuteTime: { $lte: now } });
+    const entries = await cursor.toArray();
+
+    for (const entry of entries) {
+      await unmuteUser(entry.guildId, entry.userId);
+      await collection.deleteOne({ _id: entry._id });
+    }
+  } finally {
+    await mongodb.close();
+  }
+}
+setInterval(checkUnmuteTime, 1000);
 
 // Log in to Discord
 client.login(token);
